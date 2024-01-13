7092fd6f08f5f58c457635d2f7d879fbe21f9098
Aaron <aab521@gmail.com>
2024-01-12 19:57:13 -0800

    app rendering

---
+++ client/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+
+const App = () => {
+
+
+  
+  return (
+    <div className="App">
+      <h2>
+ฅV●ᴥ●Vฅ
+<br></br><br></br>
+
+
+ₜₕₐₙₖ ᵧₒᵤ
+<br></br><br></br>
+
+     <br></br>                ʚɞ
+
+ପଓ¸¸.•*¨*•
+<br></br><br></br>
+ʕ ⊃･ ◡ ･ ʔ⊃ ━☆ﾟ.*･｡♫.*･ ʰᵃᵖᵖʸ ᵦ𝒹ₐᵧ ʰᵃᶜᵏᵉʳˢ𝓌ᵢₜₕ 𝓌ᵢₛ𝒹ₒₘ･:*☆
+<br></br><br></br>
+
+ʚɞ ⁺˖ ⸝⸝
+
+</h2>
+    </div>
+  )
+}
+
+
+export default App;
