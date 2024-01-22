352102b748bfc654afaf82767503db6fdcf0e1cb
Hay Nocik <haynocik@gmail.com>
2024-01-21 17:29:32 -0800

    added focus bar

---
+++ client/components/Focus.jsx
@@ -0,0 +1,23 @@
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import ReactFlow, {
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from 'reactflow';
+import 'reactflow/dist/style.css';
+
+
+
+
+
+
+
+
+
+
+
+
