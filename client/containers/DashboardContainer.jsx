053cb097789b046667f58a510819586402c3a3cf
Aaron <aab521@gmail.com>
2024-02-04 19:15:18 -0800

    make line chart and bar chart for alerts on dashboard

--- client/containers/DashboardContainer.jsx
+++ client/containers/DashboardContainer.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import DashTableOfTables from "../components/DashTableOfTables";
+import DashAlertLineChart from "../components/DashAlertLineChart";
+import DashAlertBarChart from "../components/DashAlertBarChart";
 
 const DashboardContainer = () => {
 
   return (
     <div>
       <h1>Dashboard</h1>
+      <DashAlertLineChart />  
+      <DashAlertBarChart />
       <DashTableOfTables />
     </div>
   )
