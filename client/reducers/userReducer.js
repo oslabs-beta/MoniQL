b4a9f52455d0fb9129c69617c01e748f7c56772c
King-Hur Wu <king2510@gmail.com>
2024-02-05 19:38:05 -0600

    merged with hay

--- client/reducers/userReducer.js
+++ client/reducers/userReducer.js
@@ -1,27 +1,11 @@
 import * as types from '../constants/actionTypes';
 
 // for dev mode:
-const initialState = {
-  user_id: 21,
-  username: 'Ebo',
-  uri: 'postgres://brcinqqs:vVcwddQCC_ebHwGbKefRAacN2dIFO0XO@bubble.db.elephantsql.com/brcinqqs',
-  isLoggedIn: true,
-};
-
-// for prod mode:
-const initialState = {
-  user_id: 21,
-  username: 'Ebo',
-  uri: 'postgres://brcinqqs:vVcwddQCC_ebHwGbKefRAacN2dIFO0XO@bubble.db.elephantsql.com/brcinqqs',
-  isLoggedIn: true,
-};
-
-// for prod mode:
 // const initialState = {
-//   user_id: null,
-//   username: null,
-//   uri: null,
-//   isLoggedIn: false,
+//   user_id: 21,
+//   username: 'Ebo',
+//   uri: 'postgres://brcinqqs:vVcwddQCC_ebHwGbKefRAacN2dIFO0XO@bubble.db.elephantsql.com/brcinqqs',
+//   isLoggedIn: true,
 // };
 
 // for prod mode:
