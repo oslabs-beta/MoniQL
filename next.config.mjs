bb00158ccfb8c47d97a299e77998b4765e010707
Hay Nocik <haynocik@gmail.com>
2024-02-07 05:55:07 -0800

    test5

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/MoniQL",
+    output: "export",  
+    reactStrictMode: true,
+};
+
+export default nextConfig;
--- splash/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"QuBvT5Mwg1/WBqVec9ZPv/lgp2xG3pVR0wArNtsYJq8=\"\n}"
